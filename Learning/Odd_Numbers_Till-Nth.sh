d38bc58bcc139e948c472880bf6d8e7433c7917a
DarksiderCrood <jhakaushtubh@gmail.com>
2021-09-26 20:48:22 +0530

    26 Sep 2021

--- Learning/Odd_Numbers_Till-Nth.sh
+++ Learning/Odd_Numbers_Till-Nth.sh
@@ -1,1 +1,11 @@
-# Your task is to use for loops to display only odd natural numbers from  to .
+#!/bin/bash
+# Your task is to use for loops to display only odd natural numbers from 1 to 99.
+
+for x in {1..99}
+do  
+    if [ $((x%2)) -ne 0 ]
+        then
+            echo "$x"
+        fi
+done
+
